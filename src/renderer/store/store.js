708f79768730f7627797daaf40cabb0965c8a653
Kevin Nam <kevinjnam@gmail.com>
2019-10-10 12:16:29 -0700

    created generatedFileTreeObject in Left Header

--- src/renderer/store/store.js
+++ src/renderer/store/store.js
@@ -6,18 +6,34 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     showFileExplorer: true,
+    fileTree: null,
+    filePath: null
   },
   mutations: {
     changeFileExplorer(state) {
       state.showFileExplorer = !state.showFileExplorer;
     },
+    updateFilePath(state, payload) {
+      state.filePath = payload.path;
+    }, 
+    updateFileTree(state, payload) {
+      state.fileTree = payload.fileTree;
+    }, 
   }, 
   actions: {
     toggleFileExplorer(context) {
       context.commit('changeFileExplorer');
     },
+    setFilePath(context, path) {
+      context.commit('updateFilePath', { path })
+    },
+    createFileTree(context, fileTree) {
+      context.commit('updateFileTree', { fileTree })
+    }
   },
   getters: {
     showFileExplorer: state => state.showFileExplorer,
+    getFileTree: state => state.fileTree,
+    getFilePath: state => state.filePath,
   }
 })
