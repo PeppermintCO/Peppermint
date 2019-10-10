4300f7bde18d2258cb9c44f21cdf9ae9b84e0e0f
Jan Spencer Catalan <jancatalan2@gmail.com>
2019-10-09 18:45:31 -0700

    saved component name to store

--- src/renderer/containers/MainLoader/MainPanel/CreateTest.vue
+++ src/renderer/containers/MainLoader/MainPanel/CreateTest.vue
@@ -1,47 +1,47 @@
 <template>
   <div class="test-name test-component">
-    <form action="">
-      Test Name <input type='text'/>
-      <button @click='addQuery'>Add Query</button>
-      <button @click='addEvent'>Add Event</button>
-      <div v-for='(testItem, index) in testItems' :key='index'>
-         <component :is=testItem></component>
-         <span :id='index' @click='deleteItem(index)'>X</span>
+    <form action>
+      Test Name
+      <input type="text" />
+      <button @click="addQuery">Add Query</button>
+      <button @click="addEvent">Add Event</button>
+      <div v-for="(testItem, index) in testItems" :key="index">
+        <component :is="testItem"></component>
+        <span :id="index" @click="deleteItem(index)">X</span>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import Query from './Query.vue';
-import FireEvent from './FireEvent.vue';
+import Query from "./Query.vue";
+import FireEvent from "./FireEvent.vue";
 
 export default {
-  name: 'CreateTest',
+  name: "CreateTest",
   components: {
     Query,
     FireEvent
   },
-  data () {
+  data() {
     return {
-      testItems: ['Query'],
-    }
+      testItems: ["Query"]
+    };
   },
   methods: {
     addQuery(e) {
       e.preventDefault();
-      this.testItems.push('Query');
+      this.testItems.push("Query");
     },
     addEvent(e) {
       e.preventDefault();
-      this.testItems.push('FireEvent');
-
+      this.testItems.push("FireEvent");
     },
     deleteItem(index) {
       this.testItems.splice(index, 1);
-    },
+    }
   }
-}
+};
 </script>
 
 <style scoped>
--- src/renderer/store/store.js
+++ src/renderer/store/store.js
@@ -6,16 +6,31 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     showFileExplorer: true,
+    componentName: '',
+    testList: {},
+    testLength: 0,
   },
   mutations: {
     changeFileExplorer(state) {
       state.showFileExplorer = !state.showFileExplorer;
     },
-  }, 
+    saveComponentName(state) {
+
+    },
+    saveCurrentTests(state) {
+
+      // for (let i = 0; i < testLength; i++) {
+
+      // }
+    }
+  },
   actions: {
     toggleFileExplorer(context) {
       context.commit('changeFileExplorer');
     },
+    saveCurrentTests(context) {
+      context.commit('SaveCurrentTest');
+    }
   },
   getters: {
     showFileExplorer: state => state.showFileExplorer,
