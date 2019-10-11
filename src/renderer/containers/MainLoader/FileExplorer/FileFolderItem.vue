683eb8455a48952483a5eeff6c9a0bedc1213350
Kevin Nam <kevinjnam@gmail.com>
2019-10-10 17:35:01 -0700

    displaying file directory by creating components for each file

---
+++ src/renderer/containers/MainLoader/FileExplorer/FileFolderItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li>{{ fileName }}</li>
+    <file-folder v-for="(file, index) in files" :key="index" :fileName="file.fileName" :files="file.files">
+
+    </file-folder>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'file-folder',
+  props: ['fileName', 'files'],
+}
+</script>
