fbc9cb7e2ed5821dbbee52324330758e1ded3565
Kevin Nam <kevinjnam@gmail.com>
2019-10-10 18:47:25 -0700

    added dblclick event listener to files

--- src/renderer/containers/MainLoader/FileExplorer/FileFolderItem.vue
+++ src/renderer/containers/MainLoader/FileExplorer/FileFolderItem.vue
@@ -1,15 +1,18 @@
 <template>
-  <ul>
-    <li :style="indent">{{ fileName }}</li>
+  <ul v-if="files.length > 0">
+    <li :style="indent" @click="toggleChildren">{{ fileName }}</li>
     <file-folder
+       v-if="showChildren"
        v-for="(file, index) in files"
        :key="index" 
        :fileName="file.fileName"
        :files="file.files"
        :depth="depth + 1" 
         >
-
     </file-folder>
+  </ul>
+  <ul v-else>
+    <li :style="indent" @dblclick="displayFileContent">{{ fileName }}</li>
   </ul>
 </template>
 
@@ -17,12 +20,25 @@
 export default {
   name: 'file-folder',
   props: ['fileName', 'files', 'depth'],
+  data() {
+    return {
+      showChildren: false
+    }
+  },
   computed: {
     indent() {
       return { 
-        transform: `translate(${this.depth * 50}px)`,
+        transform: `translate(${this.depth * 20}px)`,
         "list-style": `none`
         }
+    }
+  },
+  methods: {
+    toggleChildren() {
+      this.showChildren = !this.showChildren;
+    },
+    displayFileContent() {
+      console.log('diplaying your contents!!!!!!')
     }
   }
 }
