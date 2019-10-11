b7c079cc4bfa79cd9262713f0db24e9f812c962d
Kevin Nam <kevinjnam@gmail.com>
2019-10-11 15:27:50 -0700

    installed monaco editor web plugin npm

--- src/renderer/containers/MainLoader/RightPanel/RightPanel.vue
+++ src/renderer/containers/MainLoader/RightPanel/RightPanel.vue
@@ -1,11 +1,23 @@
 <template>
   <div class= "mainPanel-container">
-      <p>Im inside rightPanel</p>
+      <MonacoEditor class="editor" v-model="code" language="javascript" />
   </div>
 </template>
 
 <script>
+  import MonacoEditor from 'vue-monaco'
 
+  export default {
+    components: {
+      MonacoEditor
+    },
+
+    data() {
+      return {
+        code: 'const noop = () => {}'
+      }
+    }
+  }
 </script>
 
 <style>
