2bda93c0fd79ea53123ff3b7f16f7e6d7044dc65
margie <ly.margarett@gmail.com>
2019-10-11 19:22:13 -0700

    fix merge conflict in CreateTest.vue

--- src/renderer/containers/MainLoader/MainPanel/CreateTest.vue
+++ src/renderer/containers/MainLoader/MainPanel/CreateTest.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="test-name test-component">
-<<<<<<< HEAD
-    <form action="">
-      Test Name <input type='text'/>
-      <button @click='addQuery'>Add Query</button>
-      <button @click='addEvent'>Add Event</button>
-      <div v-for='(testItem, index) in testItems' :key='index'>
-         <component :is='testItem'></component>
-         <span :id='index' @click='deleteItem(index)'>X</span>
-=======
     <form action>
       Test Name
       <input name="testName" type="text" v-model="testName" @input="saveTestName" />
@@ -19,7 +10,6 @@
         <span :id="index" @click="deleteItem(index)">
           <button>X</button>
         </span>
->>>>>>> 87f670f0f07055d0c04eb52a6693ab46887572b4
       </div>
     </form>
     <span @click="deleteTest(_uid)">
