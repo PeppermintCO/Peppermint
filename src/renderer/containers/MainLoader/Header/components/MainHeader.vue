7f813707b5d5099ce76eaae2bccb3139d32fc0ce
Kevin Nam <kevinjnam@gmail.com>
2019-10-14 13:41:38 -0700

    created test directory and test file functionality on export button

--- src/renderer/containers/MainLoader/Header/components/MainHeader.vue
+++ src/renderer/containers/MainLoader/Header/components/MainHeader.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="main-header">
-    <button class="button saveCurrentButton" 
-      :disabled="hasTestInput" 
-      @click="displayTestFile">Save Current Test
+    <button class="button saveCurrentButton" @click="displayTestFile">Save Current Test
     </button>
-    {{hasTestInput}}
+    {{disableButton}}
   </div>  
 </template>
 
 <script>
 export default {
+  // data() {
+  //   return {
+  //     disableButton: true
+  //   }
+  // },
   methods: {
     displayTestFile() {
       this.$eventHub.$emit('test-content-set');
     }
   },
-  computed: {
-    hasTestInput() {
-      return Object.keys(this.$store.getters.showTestList()).length === 0;
-    }
-  },
-  watch: {
-    hasTestInput(newState, oldState) {
-      console.log(`Do you have input into test? ${newState}`);
-    }
-  }
-
+  // computed: {
+  //   hasTestInput() {
+  //     return Object.keys(this.$store.getters.showTestList).length === 0;
+  //   }
+  // },
+  // watch: {
+  //   hasTestInput(newState, oldState) {
+  //     console.log(`Do you have input into test? ${newState}`);
+  //     if(newState === false) {
+  //       this.disableButton = false;
+  //     }
+  //   }
+  // }
 }
 </script>
 
--- src/renderer/containers/MainLoader/Header/components/RightHeader.vue
+++ src/renderer/containers/MainLoader/Header/components/RightHeader.vue
@@ -3,11 +3,14 @@
     
    <button className="show-file-content button" @click="displayFileContent">View File Content</button>
    <button className="show-test-content button" @click="displayTestFile">View Test Code</button>
-   <button> Export Test</button>
+   <button className="export-test-file button" @click="exportTest"> Export Test</button>
   </div>
 </template>
 
 <script>
+const { remote } = window.require('electron');
+const electronFs = remote.require('fs');
+
   export default {
     methods: {
       displayFileContent() {
@@ -17,6 +20,37 @@
       displayTestFile() {
         console.log('inside display Test File (RIGHTHEADER)')
         this.$eventHub.$emit('test-content-set')
+      },
+      exportTest() {
+        if (!this.$store.getters.getFilePath 
+            || !this.$store.getters.showComponentName 
+            || !this.$store.getters.getTestContent) return;
+        console.log('exporting...')
+        const componentName = this.$store.getters.showComponentName
+        const filePath = this.$store.getters.getFilePath
+        const directoryName = '__test__'
+        const directoryPath = `${filePath}/${directoryName}`;
+        const fileContent = this.$store.getters.getTestContent;
+        const testFilePath = `${filePath}/${directoryName}/${componentName}.js`
+        
+       if (electronFs.existsSync(directoryPath)) {
+         electronFs.writeFile(testFilePath, fileContent, (err) => {
+           if (err) console.error(err);
+           console.log(`created new test file!`)
+         })
+       } else {
+         try {
+           electronFs.mkdirSync(directoryPath);
+           console.log('created new directory called __test__');
+
+           electronFs.writeFile(testFilePath, fileContent, (err) => {
+           if (err) console.error(err);
+           console.log(`created new test file!`)
+           })  
+         } catch (err) {
+           console.error(err)
+         }
+       }
       }
     }
   }
