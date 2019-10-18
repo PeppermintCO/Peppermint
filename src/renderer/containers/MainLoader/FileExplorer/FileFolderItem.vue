dfb90826cd33b329c2cf4a255b9550d0f173bf67
Kevin Nam <kevinjnam@gmail.com>
2019-10-18 15:43:23 -0700

    chang background color on mouse over and mouse leave. change font on file explorer

--- src/renderer/containers/MainLoader/FileExplorer/FileFolderItem.vue
+++ src/renderer/containers/MainLoader/FileExplorer/FileFolderItem.vue
@@ -1,6 +1,6 @@
 <template>
   <ul v-if="files.length > 0">
-    <li :style="indent" @click="toggleChildren">{{ fileName }}</li>
+    <li :style="indent" :class="{ active: hover }" @mouseover="hover = true" @mouseleave="hover = false" @click="toggleChildren">{{ fileName }}</li>
     <file-folder
        v-for="(file, index) in files"
        v-if="showChildren"
@@ -13,7 +13,7 @@
     </file-folder>
   </ul>
   <ul v-else>
-    <li :style="indent" @dblclick="displayFileContent(filePath)">{{ fileName }}</li>
+    <li :style="indent" :class="{ active: hover }" @mouseover="hover = true" @mouseleave="hover = false" @dblclick ="displayFileContent(filePath)">{{ fileName }}</li>
   </ul>
 </template>
 
@@ -26,7 +26,8 @@ export default {
   props: ['fileName', 'files', 'depth', 'filePath'],
   data() {
     return {
-      showChildren: false
+      showChildren: false,
+      hover: false
     }
   },
   computed: {
@@ -34,7 +35,13 @@ export default {
       return { 
         transform: `translate(${this.depth * 20}px)`,
         "list-style": `none`,
-        "color": 'skyblue'
+        "color": 'skyblue',
+        'padding-top': '3px',
+        'padding-bottom': '3px',
+        'letter-spacing': '2px',
+        'word-spacing': '3px',
+        'font-family': '"Ubuntu", sans-serif',
+        'font-size': '13.5px',
         }
     }
   },
@@ -59,5 +66,11 @@ export default {
 }
 </script>
 
-
+<style>
+  .active {
+    background-color: rgb(90, 154, 175);
+    cursor: pointer;
+    color: black; 
+  }
+</style>
 
