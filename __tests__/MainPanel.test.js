2c51cc1ad8df588a93682b756832a2a304b28c2f
kbsjsn <shin.kirk@gmail.com>
2019-10-28 10:28:01 -0700

    add img files for icons, applied changes in test for testing vuex

---
+++ __tests__/MainPanel.test.js
@@ -0,0 +1,17 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import MainPanel from  "../src/renderer/containers/MainLoader/MainPanel/MainPanel.vue"
+
+import { store } from "../src/renderer/store/store"
+
+afterEach(cleanup)
+
+function renderVuexComponent(customStore) {
+	return render(MainPanel, {store: {...store, ...customStore}});
+}
+
+test('has component name', async () => {
+	const { getByText } = renderVuexComponent();
+
+	const componentName = getByText('Component Name')
+})
+
