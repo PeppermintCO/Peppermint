3f622ccd45ab39a627ad1e14e40ba1e4ded90cf3
margie <ly.margarett@gmail.com>
2019-10-29 15:53:48 -0700

    mock store in MainPanel test

--- __tests__/MainPanel.test.js
+++ __tests__/MainPanel.test.js
@@ -1,16 +1,10 @@
 import { render, fireEvent, cleanup } from '@testing-library/vue'
 import MainPanel from  "../src/renderer/containers/MainLoader/MainPanel/MainPanel.vue"
 
-import { store } from "../src/renderer/store/store"
-
 afterEach(cleanup)
 
-function renderVuexComponent(customStore) {
-	return render(MainPanel, {store: {...store, ...customStore}});
-}
-
-xtest('has component name', async () => {
-	const { getByText } = renderVuexComponent();
+test('has component name', async () => {
+	const { getByText } = render(MainPanel, {store: { actions: { addTest() {} }}});
 
 	const componentName = getByText('Component Name')
 })
