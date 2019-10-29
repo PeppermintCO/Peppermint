349aec37f22ac565ddc831f2bb532f98517a1477
margie <ly.margarett@gmail.com>
2019-10-29 16:39:25 -0700

    add tests to MainPanel.test.js

--- __tests__/CreateTest.test.js
+++ __tests__/CreateTest.test.js
@@ -3,28 +3,26 @@ import CreateTest from  '../src/renderer/containers/MainLoader/MainPanel/CreateT
 
 afterEach(cleanup)
 
-updateProps({})
-
 test('it has a test name label', async () => {
-	const { getByText } = render(CreateTest, props: {key: value})
+	const { getByText } = render(CreateTest, {store: { actions: { addTest() {} }}})
 
 	const testNameLabel = getByText('Test Name')
 })
 
 test('it has a button to add queries', async () => {
-	const { getByText } = render(CreateTest)
+	const { getByText } = render(CreateTest, {store: { actions: { addTest() {} }}})
 
 	const addQueryButton = getByText('Add Query')
 })
 
 test('it has a button to add events', async () => {
-	const { getByText } = render(CreateTest)
+	const { getByText } = render(CreateTest, {store: { actions: { addTest() {} }}})
 
 	const addEventButton = getByText('Add Event')
 })
 
 test('it has a button to delete tests', async () => {
-	const { getByText } = render(CreateTest)
+	const { getByText } = render(CreateTest, {store: { actions: { addTest() {} }}})
 
 	const deleteTestButton = getByText('Delete Test')
 })
--- __tests__/MainPanel.test.js
+++ __tests__/MainPanel.test.js
@@ -8,3 +8,21 @@ test('has component name', async () => {
 
 	const componentName = getByText('Component Name')
 })
+
+test('it has a button to display tests', async () => {
+	const { getByText } = render(MainPanel, {store: { actions: { addTest() {} }}})
+
+	const displayTestButton = getByText('Display Test')
+})
+
+test('it has a button to add tests', async () => {
+	const { getByText } = render(MainPanel, {store: { actions: { addTest() {} }}})
+
+	const displayTestButton = getByText('Add Test')
+})
+
+test('it has a button to add props', async () => {
+	const { getByText } = render(MainPanel, {store: { actions: { addTest() {} }}})
+
+	const displayTestButton = getByText('Add Props')
+})
